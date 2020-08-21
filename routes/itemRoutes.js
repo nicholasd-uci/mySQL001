55059b89ef2f75d8a8e01fb6d14ad773230431b3
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-21 13:21:52 -0700

    updated PUT DELETE with corrrect query

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -8,7 +8,7 @@ const fs = require('fs')
 
 
 
-// Get all items
+// GET all items
 router.get('/items', (req, res) => {
     db.query('SELECT * FROM items', (err, items) => {
         if (err) { console.log(err) }
@@ -17,22 +17,28 @@ router.get('/items', (req, res) => {
 })    
 
 
-// Post one item
+// POST one item
 router.post('/items/', (req, res) => {
-    db.query('INSERT INTO items SET ?', req.body, (err, results) => {
+    db.query('INSERT INTO items SET ?', req.body, err => {
         if (err) { console.log(err) }
-        
+        res.sendStatus(200)
     })
 })
 
-// put one item
+// PUT one item
 router.put('/items/:id', (req, res) => {
-
+    db.query('/UPDATE items SET isDONE = ? WHERE id = ?', [req.body.isDone, req.params.id], err => {
+        if (err) { console.log(err) }
+        res.sendStatus(200)
+    })
 })
 
 // DELETE one item
 router.delete('/:id', (req, res) => {
-
+    db.query('DELETE FROM items WHERE id = ?', req.params.id, err => {
+        if (err) { console.log(err) }
+        res.sendStatus(200)
+    })
 })
 
 module.exports = router
