e43a9606af70d3ae7fb064d61c336574ffc1110e
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-24 23:04:39 -0700

    updated QF code

--- db/index.js
+++ db/index.js
@@ -1,19 +1,3 @@
 const mysql = require('mysql2')
 
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'rootroot',
-//     database: 'todo_db'
-// })
-
-// module.exports = db
-
-
-// you can also write it like this:
-module.exports = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'rootroot',
-    database: 'todo_db'
-})
+module.exports = mysql.createConnection(process.env.JAWSDB_URL || 'mysql://root:rootroot@localhost/todo_db')
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -3,42 +3,36 @@
 const router = require('express').Router()
 const db = require('../db')
 
-const { join } = require('path')
-const fs = require('fs')
-
-
-
 // GET all items
 router.get('/items', (req, res) => {
-    db.query('SELECT * FROM items', (err, items) => {
-        if (err) { console.log(err) }
-        res.json(items)
-    })
-})    
-
+  db.query('SELECT * FROM items', (err, items) => {
+    if (err) { console.log(err) }
+    res.json(items)
+  })
+})
 
 // POST one item
-router.post('/items/', (req, res) => {
-    db.query('INSERT INTO items SET ?', req.body, err => {
-        if (err) { console.log(err) }
-        res.sendStatus(200)
-    })
+router.post('/items', (req, res) => {
+  db.query('INSERT INTO items SET ?', req.body, (err, fields) => {
+    if (err) { console.log(err) }
+    res.json({ id: fields.insertId })
+  })
 })
 
 // PUT one item
 router.put('/items/:id', (req, res) => {
-    db.query('/UPDATE items SET isDONE = ? WHERE id = ?', [req.body.isDone, req.params.id], err => {
-        if (err) { console.log(err) }
-        res.sendStatus(200)
-    })
+  db.query('UPDATE items SET isDone = ? WHERE id = ?', [req.body.isDone, req.params.id], err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
 })
 
 // DELETE one item
-router.delete('/:id', (req, res) => {
-    db.query('DELETE FROM items WHERE id = ?', req.params.id, err => {
-        if (err) { console.log(err) }
-        res.sendStatus(200)
-    })
+router.delete('/items/:id', (req, res) => {
+  db.query('DELETE FROM items WHERE id = ?', req.params.id, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
 })
 
 module.exports = router
